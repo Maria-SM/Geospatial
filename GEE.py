92a61275e618c2836330c5d0a9185a11e3c57229
Maria.Suarez <60387852+Maria-SM@users.noreply.github.com>
2021-12-27 18:38:39 +0000

    Update GEE.py

--- GEE.py
+++ GEE.py
@@ -3,3 +3,11 @@
 #Once we have imported the product we are interested in (e.g. SRTM), we add it by typing:#
 
 Map.addLayer(srtm);
+
+#On this layer (SRTM), we can add more info
+
+#If we enter Map.addLayer and then click (Ctrl + space) the following parameters will appear:#
+Map.addLayer(eeObject, visParams, name, shown, opacity)
+##eeObject --> this parameters relates to the product we add as a new layer (e.g. SRTM)
+visParams --> This parameter relates to the visualisation of the object. Basically it determines how this will look. 
+name --> ##
